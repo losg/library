ab03622fa4b82d61e7fd232d94e372d7c9a6b15e
losg <315833683@qq.com>
2026-04-25 14:09:53 +0800

    Update BaAnimDialog.java

--- app/src/main/java/com/losg/library/widget/dialog/BaAnimDialog.java
+++ app/src/main/java/com/losg/library/widget/dialog/BaAnimDialog.java
@@ -14,7 +14,6 @@ import android.view.animation.LinearInterpolator;
 
 public abstract class BaAnimDialog extends Dialog {
 
-    private boolean mIsClosing = false;
     private View    mRootView  = null;
 
     public BaAnimDialog(Context context) {
@@ -35,56 +34,7 @@ public abstract class BaAnimDialog extends Dialog {
 
     protected abstract int initLayout();
 
-    @Override
-    public void show() {
-        super.show();
-        mIsClosing = false;
-        ObjectAnimator scaleX = ObjectAnimator.ofFloat(mRootView, "scaleX", 0.5f, 1f, 0.98f, 1f);
-        ObjectAnimator scaleY = ObjectAnimator.ofFloat(mRootView, "scaleY", 0.5f, 1f, 0.98f, 1f);
-        ObjectAnimator alpha = ObjectAnimator.ofFloat(mRootView, "alpha", 0.5f, 1f, 0.98f, 1f);
-        AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(scaleX, scaleY, alpha);
-        animatorSet.setDuration(500);
-        animatorSet.setInterpolator(new LinearInterpolator());
-        animatorSet.start();
-    }
-
     public void dismissWithoutAnim(){
         super.dismiss();
     }
-
-    @Override
-    public void dismiss() {
-        if (mIsClosing) return;
-        mIsClosing = true;
-        ObjectAnimator scaleX = ObjectAnimator.ofFloat(mRootView, "scaleX", 1f, 0.5f);
-        ObjectAnimator scaleY = ObjectAnimator.ofFloat(mRootView, "scaleY", 1f, 0.5f);
-        ObjectAnimator alpha = ObjectAnimator.ofFloat(mRootView, "alpha", 0.8f, 0f);
-        AnimatorSet animatorSet = new AnimatorSet();
-        animatorSet.playTogether(scaleX, scaleY, alpha);
-        animatorSet.setDuration(200);
-        animatorSet.start();
-        animatorSet.addListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                BaAnimDialog.super.dismiss();
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-
-            }
-        });
-    }
-
 }
